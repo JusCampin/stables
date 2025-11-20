50c619ae352faaec1bcc46f09f643e18d156837d
Apollyon <37031311+JusCampin@users.noreply.github.com>
2025-11-20 15:19:56 -0800

    Update main.lua
    - add lang
    - add devMode
    - add keys

--- configs/main.lua
+++ configs/main.lua
@@ -1,3 +1,23 @@
 Config = {
 
+    defaultlang = 'en_lang',
+    -----------------------------------------------------
+
+    devMode = true, -- Set to true to see debug prints
+    -----------------------------------------------------
+
+    keys = {
+        shop = 0x760A9C6F,    --[G] Open Stable Menu
+        -- call = 0x80F28E95,    --[L] Call Horse at Stable
+        -- ret = 0x27D1C284,     --[R] Return Horse at Stable
+        -- sell = 0x80F28E95,    --[L] Sell Tamed Horse at Sell Point
+        -- keep = 0x27D1C284,    --[R] Keep Tamed Horse at Sell Point
+        -- trade = 0x27D1C284,   --[R] Trade Horse to Player
+        -- drink = 0xD8F73058,   --[U] Horse Drink when in Water
+        -- rest = 0x620A6C5E,    --[V] Horse Rest
+        -- sleep = 0x43CDA5B0,   --[Z] Horse Sleep
+        -- wallow = 0x9959A6F0,  --[C] Horse Wallow
+        -- loot = 0x27D1C284,    --[R] Loot Horse
+    }
+-----------------------------------------------------
 }
