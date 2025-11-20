bfa0c9343706f8e0ef53f026d0e8131d8f40983c
Apollyon <37031311+JusCampin@users.noreply.github.com>
2025-11-20 14:17:12 -0800

    Create server_init.lua

---
+++ server/server_init.lua
@@ -0,0 +1,5 @@
+Core = exports.vorp_core:GetCore()
+BccUtils = exports['bcc-utils'].initiate()
+
+-- TODO: Update URL
+BccUtils.Versioner.checkFile(GetCurrentResourceName(), 'https://github.com/BryceCanyonCounty/stables')
