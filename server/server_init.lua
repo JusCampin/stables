76bf09428c7cac7b86e64b54d6c93bf263f72f89
Apollyon <37031311+JusCampin@users.noreply.github.com>
2025-11-20 15:18:28 -0800

    Update server_init.lua
    - add debug lib

--- server/server_init.lua
+++ server/server_init.lua
@@ -1,5 +1,7 @@
 Core = exports.vorp_core:GetCore()
 BccUtils = exports['bcc-utils'].initiate()
+---@type BCCStablesDebugLib
+local DBG = BCCStablesDebug
 
 -- TODO: Update URL
 BccUtils.Versioner.checkFile(GetCurrentResourceName(), 'https://github.com/BryceCanyonCounty/stables')
