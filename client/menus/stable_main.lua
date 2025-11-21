8f4d51fd56d9aced0cd18f2b7ac1cc8bdcb0d413
Apollyon <37031311+JusCampin@users.noreply.github.com>
2025-11-20 23:42:29 -0800

    Update stable_main.lua

--- client/menus/stable_main.lua
+++ client/menus/stable_main.lua
@@ -25,3 +25,36 @@ StableShopMenu = FeatherMenu:RegisterMenu('bcc-stables:shop:menu', {
         DisplayRadar(true)
     end
 })
+
+function StableMenu(shop)
+    local MainPage = StableShopMenu:RegisterPage('main:page')
+
+    if not shop or Stables[shop] then
+        DBG.Error('Invalid shop passed to StableMenu')
+        return
+    end
+
+    local shopCfg = Stables[shop]
+
+    MainPage:RegisterElement('header', {
+        value = shopCfg.shop.name,
+        slot = 'header',
+        style = {
+            ['color'] = '#999'
+        }
+    })
+
+    MainPage:RegisterElement('subheader', {
+        value = _U('horseManagement'),
+        slot = 'header',
+        style = {
+            ['font-size'] = '0.94vw',
+            ['color'] = '#CC9900'
+        }
+    })
+
+    MainPage:RegisterElement('line', {
+        slot = 'header',
+        style = {}
+    })
+end
