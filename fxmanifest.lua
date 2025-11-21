13a9c0b1f2e6aee331cb151b27d238b1ae6332f2
Apollyon <37031311+JusCampin@users.noreply.github.com>
2025-11-20 23:41:46 -0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,23 +5,33 @@ game 'rdr3'
 lua54 'yes'
 author 'BCC Team'
 
+dependency 'feather-menu'
+
 shared_scripts {
-    'configs/*.lua',
+    'configs/main.lua',
     'debug_init.lua',
+    'configs/stables.lua',
+    'configs/breeds/index.lua',
     'locale.lua',
     'languages/*.lua'
 }
 
 client_scripts {
+    'configs/horses_loader.lua',
     'client/client_init.lua',
     'client/main.lua',
     'client/menus/stable_main.lua',
+    'client/menus/feather_menu.lua',
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
     'server/server_init.lua',
     'server/main.lua'
+}
+
+files {
+    'configs/breeds/*.lua'
 }
 
 version '1.0.0'
